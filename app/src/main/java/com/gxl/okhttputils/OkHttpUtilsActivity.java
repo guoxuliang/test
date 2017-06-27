19e8bc9935cc0fede74ba2bdc2c535d257fccc30
Administrator <632977592@qq.com>
2017-06-27 12:12:42 +0800

    git创建分支

--- app/src/main/java/com/gxl/okhttputils/OkHttpUtilsActivity.java
+++ app/src/main/java/com/gxl/okhttputils/OkHttpUtilsActivity.java
@@ -2,18 +2,28 @@ package com.gxl.okhttputils;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.administrator.test.R;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
+import okhttp3.Request;
 
 /**
  * Created by Administrator on 2017/6/27 0027.
  */
 
 public class OkHttpUtilsActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = OkHttpUtilsActivity.class.getSimpleName();
     private Button get;
     private Button post;
+    private TextView mTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,7 @@ public class OkHttpUtilsActivity extends AppCompatActivity implements View.OnCli
     private void initView() {
         get=(Button)findViewById(R.id.get);
         post=(Button) findViewById(R.id.post);
+        mTv=(TextView) findViewById(R.id.mTv);
         get.setOnClickListener(this);
         post.setOnClickListener(this);
     }
@@ -32,12 +43,68 @@ public class OkHttpUtilsActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.get:
-
+                getHtml();
                 break;
             case R.id.post:
 
                 break;
         }
 
+    }
+
+    public void getHtml()
+    {
+        String url = "http://www.zhiyun-tech.com/App/Rider-M/changelog-zh.txt";
+        url="http://www.391k.com/api/xapi.ashx/info.json?key=bd_hyrzjjfb4modhj&size=10&page=1";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .id(100)
+                .build()
+                .execute(new MyStringCallback());
+    }
+    public class MyStringCallback extends StringCallback {
+        @Override
+        public void onBefore(Request request, int id)
+        {
+            setTitle("loading...");
+        }
+
+        @Override
+        public void onAfter(int id)
+        {
+            setTitle("Sample-okHttp");
+        }
+
+        @Override
+        public void onError(Call call, Exception e, int id)
+        {
+            e.printStackTrace();
+            mTv.setText("onError:" + e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id)
+        {
+            Log.e(TAG, "onResponse：complete");
+            mTv.setText("onResponse:" + response);
+
+            switch (id)
+            {
+                case 100:
+                    Toast.makeText(OkHttpUtilsActivity.this, "http", Toast.LENGTH_SHORT).show();
+                    break;
+                case 101:
+                    Toast.makeText(OkHttpUtilsActivity.this, "https", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+        @Override
+        public void inProgress(float progress, long total, int id)
+        {
+            Log.e(TAG, "inProgress:" + progress);
+//            mProgressBar.setProgress((int) (100 * progress));
+        }
     }
 }
